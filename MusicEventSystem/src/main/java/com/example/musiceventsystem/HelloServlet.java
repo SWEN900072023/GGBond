a7c14de8b80966382427df6b560dd73e33fabfee
JessiBi <linjing.biloveet@gmail.com>
2023-09-12 18:58:05 +1000

    restructure

---
+++ MusicEventSystem/src/main/java/com/example/musiceventsystem/HelloServlet.java
@@ -0,0 +1,68 @@
+package com.example.musiceventsystem;
+
+import java.io.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.annotation.*;
+import java.sql.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+    private static final String PROPERTY_JDBC_URI = "jdbc.uri";
+    private static final String PROPERTY_JDBC_USERNAME = "jdbc.username";
+    private static final String PROPERTY_JDBC_PASSWORD = "jdbc.password";
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html; charset=UTF-8");
+        request.setCharacterEncoding("utf-8");
+
+        // Get database connection information from system properties
+        String url = System.getProperty(PROPERTY_JDBC_URI);
+        String user = System.getProperty(PROPERTY_JDBC_USERNAME);
+        String pw = System.getProperty(PROPERTY_JDBC_PASSWORD);
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.addHeader("Access-Control-Max-Age", "3600");
+        response.addHeader("Access-Control-Allow-Headers", "x-requested-with, origin, content-type, accept");
+
+
+        try (Connection conn = DriverManager.getConnection(url, user, pw)) {
+            String sql = "INSERT INTO customers VALUES (?, ?);";
+            try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+                stmt.setString(1, username);
+                stmt.setString(2, password);
+                stmt.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    @Override
+    public void init() {
+        // Load database driver during initialization
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        message = "Hello World!";
+    }
+
+    public void destroy() {
+    }
+}
