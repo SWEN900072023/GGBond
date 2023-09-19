06fbd530fe42981b70d74f60a0a9eb90e8f7dd51
JessiBi <linjing.biloveet@gmail.com>
2023-09-19 14:27:58 +1000

    add models

--- MusicEventSystem/src/main/java/com/example/musiceventsystem/model/Admin.java
+++ MusicEventSystem/src/main/java/com/example/musiceventsystem/model/Admin.java
@@ -1,57 +1,8 @@
 package com.example.musiceventsystem.model;
 
-public class Admin {
+public class Admin extends WebUser{
+
     public Admin(Integer id, String username, String password, String name, String telephone) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-        this.name = name;
-        this.telephone = telephone;
-    }
-
-    private Integer id;
-    private String username;
-    private String password;
-    private String name;
-    private String telephone;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
+        super(id, username, password, name, telephone);
     }
 }
--- MusicEventSystem/src/main/java/com/example/musiceventsystem/model/Order.java
+++ MusicEventSystem/src/main/java/com/example/musiceventsystem/model/Order.java
@@ -1,4 +1,107 @@
-package com.example.musiceventsystem.model;
+package com.musiceventms.mes.model;
 
 public class Order {
+    private Integer id;
+    private Integer customerId;
+    private Integer ticketId;
+    private Integer eventId;
+    private String eventName;
+    private String section;
+    private Integer price;
+    private Integer num;
+    private String time;
+
+    public Order(Integer customerId, Integer ticketId, Integer eventId, String eventName, String section, Integer price, Integer num) {
+        this.customerId = customerId;
+        this.ticketId = ticketId;
+        this.eventId = eventId;
+        this.eventName = eventName;
+        this.section = section;
+        this.price = price;
+        this.num = num;
+    }
+
+    public Order(Integer id, Integer customerId, Integer ticketId, Integer eventId, String eventName, String section, Integer price, Integer num, String time) {
+        this.id = id;
+        this.customerId = customerId;
+        this.ticketId = ticketId;
+        this.eventId = eventId;
+        this.eventName = eventName;
+        this.section = section;
+        this.price = price;
+        this.num = num;
+        this.time = time;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
+
+    public Integer getTicketId() {
+        return ticketId;
+    }
+
+    public void setTicketId(Integer ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public Integer getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(Integer eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    public void setSection(String section) {
+        this.section = section;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Integer getNum() {
+        return num;
+    }
+
+    public void setNum(Integer num) {
+        this.num = num;
+    }
 }
